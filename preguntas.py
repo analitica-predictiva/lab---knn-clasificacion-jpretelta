cf056c774d9d3fceeb6191437326ffe4e2f0347a
jpretelta <jjpretelta@unal.edu.co>
2022-11-30 12:38:23 -0500

    Changing Neighbors

--- preguntas.py
+++ preguntas.py
@@ -65,7 +65,7 @@ def pregunta_02():
     from sklearn.neighbors import KNeighborsClassifier
         
     # Cree un un clasificador k-NN con 6 vecinos
-    knn = KNeighborsClassifier(n_neighbors=6)
+    knn = KNeighborsClassifier(n_neighbors=5)
     
     # Entrene el clasificador con el conjunto de entrenamiento
     knn.fit(X,y)
@@ -77,4 +77,4 @@ def pregunta_02():
     from sklearn.metrics import confusion_matrix
 
     # Retorne la matriz de confusión
-    return confusion_matrix(y_true=y,y_pred=y_pred)
+    return confusion_matrix(y_true=y, y_pred=y_pred)
